d071afe17b7a114cc7bf2b0bc1e3c5e0d8d9d067
nitai <nitai.barran@enquiretechit.com>
2023-03-19 18:28:23 -0400

    gonna work on the messages component

---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,19 @@
+import {View, Text, ImageBackground} from "react-native";
+import {StyleSheet} from "react-native";
+import bg from '../../assets/images/BG.png';
+
+const ChatScreen = () => {
+    return (
+        <ImageBackground source={bg} style={styles.bg}>
+            <Text>ChatScreen</Text>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    bg: {
+        flex: 1,
+    },
+})
+
+export default ChatScreen;
