09634c206f43a3235ee640b5fa5c71620631bcac
nitai <nitai.barran@enquiretechit.com>
2023-03-19 19:02:38 -0400

    chat with messages done

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,38 @@
 import {View, Text, ImageBackground} from "react-native";
 import {StyleSheet} from "react-native";
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from "dayjs";
+
+dayjs.extend(relativeTime)
 
 const Message = ({message}) => {
+    const isMyMessage = () => message.user.id === 'u1';
+
     return (
-        <View>
+        <View style={[
+            styles.container,
+            {
+                backgroundColor: isMyMessage() ? '#DCF8C5' : 'white',
+                alignSelf: isMyMessage() ? 'flex-end' : 'flex-start',
+            }
+        ]}>
             <Text>{message.text}</Text>
+            <Text style={styles.time}>{dayjs(message.createdAt).fromNow(true)}</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        margin: 5,
+        padding: 10,
+        borderRadius: 10,
+        maxWidth: '80%',
+    },
+    time: {
+        color: 'gray',
+        alignSelf: 'flex-end',
+    }
+})
 
 export default Message;
