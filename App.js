f9bd76a8e15ce206025696c4c15535399a6ed59b
nitai <nitai.barran@enquiretechit.com>
2023-03-19 17:26:35 -0400

    completed the chat list item

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './src/components/ChatListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Nitai</Text>
+      <ChatListItem />
+      <ChatListItem />
       <StatusBar style="auto" />
     </View>
   );
