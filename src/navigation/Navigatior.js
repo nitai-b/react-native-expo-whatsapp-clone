d2f39d5d8c226bc7d120dbe182c08e4870755dab
nitai <nitai.barran@enquiretechit.com>
2023-03-20 14:02:18 -0400

    bottom navigation is done. we needed to nest the tab navigator on the within the main navigator and pass the main navigator some options to not show the header on the home screen route

--- src/navigation/Navigatior.js
+++ src/navigation/Navigatior.js
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import ChatScreen from '../screens/ChatScreen';
 import ChatsScreen from '../screens/ChatsScreen';
+import MainTabNavigator from './MainTabNavigatior';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +11,8 @@ const Navigator = ({message}) => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name="Chats" component={ChatsScreen}></Stack.Screen>
-				<Stack.Screen name="Chat" component={ChatScreen}></Stack.Screen>
+				<Stack.Screen name="Home" component={MainTabNavigator} options={{headerShown: false}}/>
+				<Stack.Screen name="Chat" component={ChatScreen}/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
