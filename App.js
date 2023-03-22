0021721d84872932d0c79979aa1f5638437d8a5f
nitai <nitai.barran@enquiretechit.com>
2023-03-22 12:33:55 -0400

    formatting and setting up the authentication with aws amplify

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
-import ChatScreen from './src/screens/ChatScreen';
-import Navigator from "./src/navigation/Navigatior";
+import Navigator from './src/navigation/Navigatior';
+import {Amplify} from 'aws-amplify';
+import awsconfig from './src/aws-exports';
 
+Amplify.configure(awsconfig);
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Navigator/>
-            <StatusBar style="auto"/>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Navigator/>
+			<StatusBar style="auto"/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'whitesmoke',
-        justifyContent: 'center',
-    },
+	container: {
+		flex: 1,
+		backgroundColor: 'whitesmoke',
+		justifyContent: 'center',
+	},
 });
