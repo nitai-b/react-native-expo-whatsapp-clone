93dd522bb8a0e9410e248b15c16edb0d8324128c
nitai <nitai.barran@enquiretechit.com>
2023-03-24 15:59:38 -0400

    some changes to the contacts screen using the graph ql api

--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -1,14 +1,23 @@
+import {useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
 import chats from '../../assets/data/chats.json';
-import ChatListItem from '../components/ChatListItem';
 import ContactListItem from '../components/ContactListItem';
+import {API, graphqlOperation} from 'aws-amplify';
+import {listUsers} from '../graphql/queries';
 
 const ContactsScreen = () => {
+	const [users, setUsers] = useState([]);
+	useEffect(() => {
+		API.graphql(graphqlOperation(listUsers)).then((result) => {
+			console.log(result);
+			setUsers(result.data?.listUsers?.items);
+		});
+	}, []);
 	return (
 		<FlatList
 			data={chats}
-			renderItem={({item}) => <ContactListItem user={item.user}/>}
-			style={{backgroundColor: 'whitesmoke'}}
+			renderItem={({ item }) => <ContactListItem user={item}/>}
+			style={{ backgroundColor: 'whitesmoke' }}
 		/>
 	);
 };
