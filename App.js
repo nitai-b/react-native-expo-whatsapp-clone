b079b94238a93494df4967fe9f7e07cd45c471d9
nitai <nitai.barran@enquiretechit.com>
2023-03-24 17:50:07 -0400

    cannot get past this error, sigh

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {createUser} from './src/graphql/mutations';
+import {getUser} from './src/graphql/queries';
 import Navigator from './src/navigation/Navigatior';
-import {Amplify} from 'aws-amplify';
+import {Amplify, API, Auth, graphqlOperation} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native/src';
 import awsconfig from './src/aws-exports';
 
-Amplify.configure({...awsconfig, Analytics: {disabled: true}});
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 function App() {
+	const syncUser = async () => {
+		try {
+			// get Auth user
+			const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
+			const sub = authUser.attributes.sub;
+			
+			// query the database using Auth user id (sub)
+			const userData = await API.graphql(graphqlOperation(getUser, { id: sub }));
+			
+			if (userData.data.getUser) {
+				console.log('User already exists in DB');
+				return;
+			}
+			
+			const variables = {
+				id: sub,
+				name: authUser.attributes.phone_number,
+				status: 'Hey there! I am using WhatsApp',
+			};
+			console.log({ variables: variables });
+			
+			// if there is no users in db, create one
+			const newUserResponse = await API.graphql(
+				graphqlOperation(createUser, { input: variables }),
+			);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+	
+	useEffect(() => {
+		syncUser();
+	}, []);
+	
 	return (
 		<View style={styles.container}>
 			<Navigator/>
