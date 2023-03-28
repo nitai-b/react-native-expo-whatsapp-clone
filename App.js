3f7427a6a9b7b7df6974c35b6e531f23e0d6ef3f
nitai <nitai.barran@enquiretechit.com>
2023-03-28 12:14:13 -0400

    sorted the users error

--- App.js
+++ App.js
@@ -5,44 +5,42 @@ import {createUser} from './src/graphql/mutations';
 import {getUser} from './src/graphql/queries';
 import Navigator from './src/navigation/Navigatior';
 import {Amplify, API, Auth, graphqlOperation} from 'aws-amplify';
-import {withAuthenticator} from 'aws-amplify-react-native/src';
+import {withAuthenticator} from 'aws-amplify-react-native';
 import awsconfig from './src/aws-exports';
 
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 function App() {
 	const syncUser = async () => {
-		try {
-			// get Auth user
-			const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
-			const sub = authUser.attributes.sub;
-			
-			// query the database using Auth user id (sub)
-			const userData = await API.graphql(graphqlOperation(getUser, { id: sub }));
-			
-			if (userData.data.getUser) {
-				console.log('User already exists in DB');
-				return;
-			}
-			
-			if (sub == null && authUser.attributes.phone_number == null) {
-				console.log('Cannot create user in Users table');
-				return;
-			}
-			
-			const newUser = {
-				id: sub,
-				name: authUser.attributes.phone_number,
-				status: 'Hey there! I am using WhatsApp',
-			};
-			
-			// if there is no users in db, create one
-			const newUserResponse = await API.graphql(
-				graphqlOperation(createUser, { input: newUser }),
-			);
-		} catch (e) {
-			console.log(e);
+		// get Auth user
+		const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
+		const sub = authUser.attributes.sub;
+		console.log(sub);
+		// query the database using Auth user id (sub)
+		const userData = await API.graphql(graphqlOperation(getUser, { id: sub }));
+		
+		console.log(authUser.attributes.phone_number == undefined);
+		if (userData.data.getUser) {
+			console.log('User already exists in DB');
+			return;
 		}
+		
+		const validPhoneNumber = authUser.attributes.phone_number == undefined || authUser.attributes.phone_number == null;
+		if (sub == null && validPhoneNumber) {
+			console.log('Cannot create user in Users table');
+			return;
+		}
+		
+		const newUser = {
+			id: sub,
+			name: validPhoneNumber ? authUser.attributes.phone_number : 'some-random-name',
+			status: 'Hey there! I am using WhatsApp',
+		};
+		
+		// if there is no users in db, create one
+		const newUserResponse = await API.graphql(
+			graphqlOperation(createUser, { input: newUser }),
+		);
 	};
 	
 	useEffect(() => {
