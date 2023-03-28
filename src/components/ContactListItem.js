67e5b59839bfd2c5a359fc990610da8b4e53697e
nitai <nitai.barran@enquiretechit.com>
2023-03-28 12:42:34 -0400

    finished the creation of a chatroom and navigating to it

--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -1,15 +1,55 @@
+import {API, Auth, graphqlOperation} from 'aws-amplify';
 import {Text, View, Image, StyleSheet, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import dayjs from 'dayjs';
+import {createChatRoom, createUserChatRoom} from '../graphql/mutations';
 
 dayjs.extend(relativeTime);
 
 const ContactListItem = ({ user }) => {
 	const navigation = useNavigation();
+	const chatRoomInit = async () => {
+		console.warn('pressed');
+		// check if we already have a chatroom with this user
+		
+		// Create a new ChatRoom
+		const newChatRoomData = await API.graphql(graphqlOperation(createChatRoom, { input: {} }));
+		console.log(newChatRoomData);
+		
+		if (!newChatRoomData.data.createChatRoom) {
+			console.log('Error createign the chatroom');
+		}
+		const newChatRoom = newChatRoomData.data?.createChatRoom;
+		
+		// Add the clicked user to the ChatRoom
+		const addUser = await API.graphql(graphqlOperation(createUserChatRoom, {
+			input: {
+				chatRoomId: newChatRoom.id,
+				userId: user.id,
+			},
+		}));
+		
+		// Add the authUser to the ChatRoom
+		const authUser = await Auth.currentAuthenticatedUser();
+		const addAuthUser = await API.graphql(graphqlOperation(createUserChatRoom, {
+			input: {
+				chatRoomId: newChatRoom.id,
+				userId: authUser.attributes.sub,
+			},
+		}));
+		
+		// navigate to the newly created chatroom
+		navigation.navigate('Chat', {
+			id: newChatRoom.id,
+		});
+	};
+	
 	return (
-		<Pressable onPress={() => {
-		}} style={styles.container}>
+		<Pressable
+			onPress={() => chatRoomInit()}
+			style={styles.container}>
+			
 			<Image style={styles.image} source={{ uri: user.image }}/>
 			
 			<View style={styles.content}>
@@ -22,7 +62,8 @@ const ContactListItem = ({ user }) => {
 				</Text>
 			</View>
 		
-		</Pressable>);
+		</Pressable>
+	);
 };
 
 const styles = StyleSheet.create({
