6a529d4a976eb690018f84e7e63dad420ba9549c
nitai <nitai.barran@enquiretechit.com>
2023-03-28 15:02:53 -0400

    changes to include querying the backend for a list of chats that belong to the authenticated user

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,61 +1,65 @@
-import {Text, View, Image, StyleSheet, Pressable} from 'react-native'
-import { useNavigation } from '@react-navigation/native';
+import {Text, View, Image, StyleSheet, Pressable} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
-const ChatListItem = ({chat}) => {
-    const navigation = useNavigation();
-    return (
-        <Pressable onPress={() => navigation.navigate('Chat', { id: chat.id, name: chat.user.name })} style={styles.container}>
-            <Image style={styles.image} source={{uri: chat.user.image}}/>
-            <View style={styles.content}>
-                <View style={styles.row}>
-                    <Text numberOfLines={1} style={styles.name}>
-                        {chat.user.name}
-                    </Text>
-                    <Text style={styles.subTitle}>
-                        {dayjs(chat.lastMessage.createdAt).fromNow(true)}
-                    </Text>
-                </View>
-                <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
-            </View>
-        </Pressable>);
-}
+const ChatListItem = ({ chat }) => {
+	const navigation = useNavigation();
+	
+	// loop through chat users items and find the user that isn't the authenticated user
+	const user = chat.users.items[0].user;
+	
+	return (
+		<Pressable onPress={() => navigation.navigate('Chat', { id: chat.id, name: user?.name })} style={styles.container}>
+			<Image style={styles.image} source={{ uri: user?.image }}/>
+			<View style={styles.content}>
+				<View style={styles.row}>
+					<Text numberOfLines={1} style={styles.name}>
+						{user?.name}
+					</Text>
+					<Text style={styles.subTitle}>
+						{dayjs(chat.lastMessage?.createdAt).fromNow(true)}
+					</Text>
+				</View>
+				<Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage?.text}</Text>
+			</View>
+		</Pressable>);
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        alignItems: "stretch",
-        marginHorizontal: 10,
-        marginVertical: 5,
-        height: 70,
-    },
-    image: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        marginRight: 10,
-    },
-    content: {
-        flex: 1,
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        borderBottomColor: "lightgray",
-        flexDirection: "column",
-        marginHorizontal: 6,
-    },
-    row: {
-        flexDirection: 'row',
-        marginBottom: 5,
-    },
-    name: {
-        flex: 1,
-        fontWeight: 'bold',
-    },
-    subTitle: {
-        color: 'grey'
-    },
-})
+	container: {
+		flexDirection: 'row',
+		alignItems: 'stretch',
+		marginHorizontal: 10,
+		marginVertical: 5,
+		height: 70,
+	},
+	image: {
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		marginRight: 10,
+	},
+	content: {
+		flex: 1,
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderBottomColor: 'lightgray',
+		flexDirection: 'column',
+		marginHorizontal: 6,
+	},
+	row: {
+		flexDirection: 'row',
+		marginBottom: 5,
+	},
+	name: {
+		flex: 1,
+		fontWeight: 'bold',
+	},
+	subTitle: {
+		color: 'grey',
+	},
+});
 
 export default ChatListItem;
--- src/screens/ChatsScreen.js
+++ src/screens/ChatsScreen.js
@@ -1,22 +1,22 @@
 import {API, Auth, graphqlOperation} from 'aws-amplify';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
-import chats from '../../assets/data/chats.json';
 import {listChats} from './queries';
 import ChatListItem from '../components/ChatListItem';
 
 const ChatsScreen = () => {
+	const [chatRooms, setChatRooms] = useState([]);
 	
 	const getChatRooms = async () => {
 		const authUser = await Auth.currentAuthenticatedUser();
 		console.log(authUser.attributes.sub);
 		
-		console.log('is null?', authUser.attributes.sub == null || false || authUser.attributes.sub == '')
+		console.log('is null?', authUser.attributes.sub == null || false || authUser.attributes.sub == '');
 		
 		const response = await API.graphql(
 			graphqlOperation(listChats, { id: authUser.attributes.sub }),
 		);
-		console.log(response);
+		setChatRooms(response.data.getUser.ChatRooms.items);
 	};
 	
 	useEffect(() => {
@@ -25,8 +25,8 @@ const ChatsScreen = () => {
 	
 	return (
 		<FlatList
-			data={chats}
-			renderItem={({ item }) => <ChatListItem chat={item}/>}
+			data={chatRooms}
+			renderItem={({ item }) => <ChatListItem chat={item.chatRoom}/>}
 			style={{ backgroundColor: 'whitesmoke' }}
 		/>
 	);
