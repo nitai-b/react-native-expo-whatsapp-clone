42e39dc1a2855bd3ebe6014855cd689ff99813da
nitai <nitai.barran@enquiretechit.com>
2023-04-03 15:31:23 -0400

    selectable and selected implemented

--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -1,3 +1,4 @@
+import {AntDesign, FontAwesome } from '@expo/vector-icons';
 import {Text, View, Image, StyleSheet, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -5,27 +6,43 @@ import dayjs from 'dayjs';
 
 dayjs.extend(relativeTime);
 
-const ContactListItem = ({ user , onPress = () => {}}) => {
-	return (
-		<Pressable
-			onPress={onPress}
-			style={styles.container}>
-			
-			<Image style={styles.image} source={{ uri: user.image }}/>
-			
-			<View style={styles.content}>
-				<Text numberOfLines={1} style={styles.name}>
-					{user.name}
-				</Text>
+const ContactListItem =
+	({
+		 user,
+		 onPress = () => {
+		 },
+		 selectable = false,
+		 isSelected = false,
+	 }) => {
+	
+	
+	
+		return (
+			<Pressable
+				onPress={onPress}
+				style={styles.container}>
 				
-				<Text numberOfLines={1} style={styles.subTitle}>
-					{user.status}
-				</Text>
-			</View>
-		
-		</Pressable>
-	);
-};
+				<Image style={styles.image} source={{ uri: user.image }}/>
+				
+				<View style={styles.content}>
+					<Text numberOfLines={1} style={styles.name}>
+						{user.name}
+					</Text>
+					
+					<Text numberOfLines={1} style={styles.subTitle}>
+						{user.status}
+					</Text>
+				</View>
+				{selectable && (
+					isSelected ? (
+						<AntDesign name="checkcircle" size={24} color="royalblue"/>
+					) : (
+						<FontAwesome name="circle-thin" size={24} color="lightgray"/>
+					)
+				)}
+			</Pressable>
+		);
+	};
 
 const styles = StyleSheet.create({
 	container: {
